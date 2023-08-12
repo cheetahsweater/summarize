88e55fb42ceecef9be0733998c479bd55ed7d8c3
Ana-Luisa Aikman <aikmanfamily@gmail.com>
2023-08-12 18:30:10 -0500

    Set up for package integration

--- summarize.py
+++ summarize.py
@@ -2,11 +2,9 @@ import openai
 import tiktoken
 import numpy as np
 import os
-import sumsecrets
+from summarize import sumsecrets
 
 openai.api_key = sumsecrets.myAPIkey
-
-token_breaker = 3000 #Token limit for OpenAI API
 encoding_break_status = False #Don't encode by default
 path = os.getcwd()
 filename = "20230730.txt"
@@ -15,7 +13,7 @@ filename = "20230730.txt"
 The following function is used to process a file that takes up more tokens than is allowed,
 Returns the rest of the file's content (rest_content) that hasn't been added to the matrix.
 '''
-def encoding_break(transcript):
+def encoding_break(transcript, token_breaker):
     global encoding_break_status
     num = len(transcript) // token_breaker
     array = np.zeros((num,token_breaker), dtype=np.int64)
@@ -30,25 +28,26 @@ def encoding_break(transcript):
     encoding_break_status = True
     return array, rest_content
 
-#Opens transcript file and loads encoding model
-transcript = open(f"{path}\\{filename}", "r").read()
-print('Transcript received!')
-encode = tiktoken.encoding_for_model('gpt-3.5-turbo')
-encode_list = encode.encode(transcript)
-print(f"The token number for the transcript is {len(encode_list)} tokens.")
-
-'''
-Check to see if the transcript is over our token limit, and if it is, slice it up to form a new numpy matrix
-if it is within the token limit, put it as it is into a matrix.
-'''
-if len(encode_list) > token_breaker:
-    final_list = encoding_break(encode_list)[0]
-    remain_content = encoding_break(encode_list)[1]
-    print("Separation process initiated...")
-else:
-    final_list = np.zeros((1,len(encode_list)), dtype=np.int64)
-    final_list[0] = encode_list
-    print("No separation needed!")
+def open_file(path, filename, token_breaker):
+    #Opens transcript file and loads encoding model
+    transcript = open(f"{path}\\{filename}", "r").read()
+    print('Transcript received!')
+    encode = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    encode_list = encode.encode(transcript)
+    print(f"The token number for the transcript is {len(encode_list)} tokens.")
+    '''
+    Check to see if the transcript is over our token limit, and if it is, slice it up to form a new numpy matrix
+    if it is within the token limit, put it as it is into a matrix.
+    '''
+    if len(encode_list) > token_breaker:
+        final_list = encoding_break(encode_list, 3000)[0]
+        remain_content = encoding_break(encode_list, 3000)[1]
+        print("Separation process initiated...")
+    else:
+        final_list = np.zeros((1,len(encode_list)), dtype=np.int64)
+        final_list[0] = encode_list
+        print("No separation needed!")
+    return final_list, remain_content
 
 '''
 Starting to pass the arguments into the GPT model
@@ -56,14 +55,12 @@ This is modified from the original one--instead of summarizing each slice of the
 which could result in missing context between slices if it's slic
 '''
 
-message_history = [{"role": "assistant", "content": f"OK"}]
-prompt1 = "Please summarize this in a way that is useful to somebody looking back at previous conversations to remember what subjects were discussed, with a list of subjects, each subject entry supplemented with a quick summary of the discussion of each subject"
-prompt2 = "I have a conversation split into multiple parts. Please summarize each part in a way that is useful to somebody looking back at previous conversations to remember what subjects were discussed, by listing subjects covered and then supplementing each subject with a summary of the things said about each subject, in a NON-NUMBERED format (for example, using dashes instead of numbers to precede each subject)"
 
 '''
 For the following function, we pass our transcripts(sliced up) in the matrix into the gpt-3.5 models one by one
 '''
-def GPT(input):
+def GPT(input, prompt1):
+    message_history = [{"role": "assistant", "content": f"OK"}]
     # tokenize the new input sentence
     message_history.append({"role": "user", "content": f"{prompt1}: {input}"}) # It is up to you to ask the model to output bullet points or just a general summary
     prompt_history = [message_history[len(message_history)-2],message_history[len(message_history)-1]] # I believe by putting the previous messages into the current context can improve the model's overall accuracy.
@@ -76,7 +73,8 @@ def GPT(input):
     message_history.append({"role": "assistant", "content": f"{reply_content}"})
     return reply_content
 
-def GPTsplitfirst(input):
+def GPTsplitfirst(input, prompt2):
+    message_history = [{"role": "assistant", "content": f"OK"}]
     # tell chatGPT that the split transcript will be sent in multiple parts and send the first part
     message_history.append({"role": "user", "content": f"{prompt2}: {input}"}) # It is up to you to ask the model to output bullet points or just a general summary
     prompt_history = [message_history[len(message_history)-2],message_history[len(message_history)-1]] # I believe by putting the previous messages into the current context can improve the model's overall accuracy.
@@ -90,6 +88,7 @@ def GPTsplitfirst(input):
     return reply_content
 
 def GPTsplitrest(input):
+    message_history = [{"role": "assistant", "content": f"OK"}]
     # send the rest of the parts of the split transcript
     message_history.append({"role": "user", "content": f"Next part to summarize: {input}"}) # It is up to you to ask the model to output bullet points or just a general summary
     prompt_history = [message_history[len(message_history)-2],message_history[len(message_history)-1]] # I believe by putting the previous messages into the current context can improve the model's overall accuracy.
@@ -105,25 +104,27 @@ def GPTsplitrest(input):
 '''
 We can then sum up all the processed summarization to a str(final_sum)
 '''
-final_sum = 'Summary\n\n'
-if encoding_break_status is True: #Check to see if we used the encoding_break function, if true, we process transcripts one by one. Otherwise, we just feed the original transcript.
-    for i in range(len(final_list)):
-        if i == 0:
-            print(f'Processing paragraph {i + 1}...')
-            final_sum += "Part 1:\n"
-            final_sum += GPTsplitfirst(encode.decode(final_list[i]))
-        else:
-            print(f'Processing paragraph {i + 1}...')
-            final_sum += f"\n\nPart {i + 1}:\n"
-            final_sum += GPTsplitrest(encode.decode(final_list[i]))
-    print(f'Processing the last paragraph...')
-    final_sum += f"\n\nFinal part:\n"
-    final_sum += GPTsplitrest(encode.decode(remain_content[0]))
-else:
-    print(f'Processing the transcript...')
-    final_sum +=GPT(encode.decode(final_list[0]))
+def summarize(final_list):
+    encode = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    final_sum = 'Summary\n\n'
+    if encoding_break_status is True: #Check to see if we used the encoding_break function, if true, we process transcripts one by one. Otherwise, we just feed the original transcript.
+        for i in range(len(final_list)):
+            if i == 0:
+                print(f'Processing paragraph {i + 1}...')
+                final_sum += "Part 1:\n"
+                final_sum += GPTsplitfirst(encode.decode(final_list[i]))
+            else:
+                print(f'Processing paragraph {i + 1}...')
+                final_sum += f"\n\nPart {i + 1}:\n"
+                final_sum += GPTsplitrest(encode.decode(final_list[i]))
+        print(f'Processing the last paragraph...')
+        final_sum += f"\n\nFinal part:\n"
+        final_sum += GPTsplitrest(encode.decode(remain_content[0]))
+    else:
+        print(f'Processing the transcript...')
+        final_sum +=GPT(encode.decode(final_list[0]))
 
-with open(f"{path}\\summary.txt", "w", encoding="utf-8-sig") as file:
-        text = file.write(final_sum)
-        file.close()
+    with open(f"{path}\\summary.txt", "w", encoding="utf-8-sig") as file:
+            text = file.write(final_sum)
+            file.close()
 
